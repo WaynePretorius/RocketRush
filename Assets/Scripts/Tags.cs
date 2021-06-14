8ed7061eac21b944d20a07cf774f7c85674f0428
Wayne-PC\Wayne <wayne007pretorius@gmail.com>
2021-06-14 15:47:53 +0200

    Initital Push

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Tags
+{
+    //THIS STATIC CLASS WILL BE USED INSTEAD OF USING STRINGS IN ANY OF SCRIPS, STRINGS WILL BE CALLED FROM HERE
+}
